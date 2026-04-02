7df9f6ddf3a5caf607230838add202046d7ff565
penep0 <pengco681@gmail.com>
2026-04-02 10:48:08 +0900

    feat: 입찰하기 화면 구현

--- src/components/AuctionDetailScreen.vue
+++ src/components/AuctionDetailScreen.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = defineProps({
   assets: {
@@ -22,18 +22,87 @@ const reportTypes = [
   '도난물 의심',
   '기타',
 ]
+const bidHistoryRows = [
+  '1번 입찰자',
+  '2번 입찰자',
+  '3번 입찰자',
+  '4번 입찰자',
+  '5번 입찰자',
+  '6번 입찰자',
+  '1번 입찰자',
+  '2번 입찰자',
+  '2번 입찰자',
+  '2번 입찰자',
+  '7번 입찰자',
+  '8번 입찰자',
+  '1번 입찰자',
+  '10번 입찰자',
+].map((bidder) => ({
+  bidder,
+  amount: '***원',
+  date: '20xx/xx/xx xx:xx:xx.xx',
+}))
 
+function parseAmount(value) {
+  return Number(String(value).replace(/[^\d]/g, '')) || 0
+}
+
+function formatAmount(value) {
+  return new Intl.NumberFormat('ko-KR').format(value)
+}
+
+const minimumBidAmount = computed(() => parseAmount(props.item.price) + parseAmount(props.item.bidUnit))
+const buyNowAmount = computed(() => parseAmount(props.item.buyNowPrice))
+
+const isBidModalOpen = ref(false)
+const isBidHistoryDrawerOpen = ref(false)
 const isReportModalOpen = ref(false)
 const reportForm = ref({
   type: '기타',
   detail: '',
 })
+const bidAmount = ref('')
 const isInquiryModalOpen = ref(false)
 const inquiryForm = ref({
   isPrivate: true,
   title: '',
   content: '',
 })
+
+function syncBidAmount(value = minimumBidAmount.value) {
+  bidAmount.value = formatAmount(value)
+}
+
+function openBidModal() {
+  isBidHistoryDrawerOpen.value = false
+  syncBidAmount()
+  isBidModalOpen.value = true
+}
+
+function closeBidModal() {
+  isBidModalOpen.value = false
+}
+
+function stepBid(direction) {
+  const nextAmount = parseAmount(bidAmount.value || minimumBidAmount.value) + parseAmount(props.item.bidUnit) * direction
+  syncBidAmount(Math.max(minimumBidAmount.value, nextAmount))
+}
+
+function submitBid() {
+  isBidModalOpen.value = false
+}
+
+function buyNow() {
+  isBidModalOpen.value = false
+}
+
+function openBidHistoryDrawer() {
+  isBidHistoryDrawerOpen.value = true
+}
+
+function closeBidHistoryDrawer() {
+  isBidHistoryDrawerOpen.value = false
+}
 
 function openReportModal() {
   isReportModalOpen.value = true
@@ -164,7 +233,7 @@ function submitInquiry() {
               <span>입찰 금액</span>
               <input type="text" :value="item.price" />
             </label>
-            <button type="button" class="detail-bid-button">지금 입찰하기</button>
+            <button type="button" class="detail-bid-button" @click="openBidModal">지금 입찰하기</button>
           </div>
 
           <div class="detail-stats">
@@ -176,7 +245,10 @@ function submitInquiry() {
         </div>
 
         <div class="history-panel">
-          <div class="history-heading">입찰 기록</div>
+          <div class="history-header">
+            <div class="history-heading">입찰 기록</div>
+            <button type="button" class="history-view-all" @click="openBidHistoryDrawer">View All</button>
+          </div>
           <div class="history-list">
             <div v-for="(row, index) in item.history" :key="`${row.bidder}-${index}`" class="history-row">
               <span>{{ row.bidder }}</span>
@@ -185,9 +257,105 @@ function submitInquiry() {
             </div>
           </div>
         </div>
-
-        <button type="button" class="back-to-list" @click="$emit('back')">목록으로</button>
       </div>
+    </div>
+
+    <div
+      v-if="isBidHistoryDrawerOpen"
+      class="detail-history-drawer-overlay"
+      @click.self="closeBidHistoryDrawer"
+    >
+      <aside class="detail-history-drawer">
+        <div class="detail-history-drawer-top">
+          <button type="button" class="detail-history-drawer-close" @click="closeBidHistoryDrawer">
+            ×
+          </button>
+        </div>
+
+        <div class="detail-history-drawer-summary">
+          <img :src="props.assets.listWatchImage" :alt="item.title" class="detail-inquiry-thumb" />
+          <div class="detail-inquiry-summary-copy">
+            <strong>{{ item.title }}</strong>
+            <span>현재 입찰가</span>
+            <em>{{ item.price }}원</em>
+          </div>
+        </div>
+
+        <div class="detail-history-table">
+          <div class="detail-history-table-head">
+            <span>입찰자</span>
+            <span>입찰액</span>
+            <span>입찰 일시</span>
+          </div>
+
+          <div class="detail-history-table-body">
+            <div
+              v-for="(row, index) in bidHistoryRows"
+              :key="`${row.bidder}-${index}`"
+              class="detail-history-table-row"
+            >
+              <span>{{ row.bidder }}</span>
+              <strong>{{ row.amount }}</strong>
+              <span>{{ row.date }}</span>
+            </div>
+          </div>
+        </div>
+
+        <button type="button" class="detail-history-action" @click="openBidModal">
+          입찰하기
+        </button>
+      </aside>
+    </div>
+
+    <div v-if="isBidModalOpen" class="detail-inquiry-overlay" @click.self="closeBidModal">
+      <section class="detail-bid-modal">
+        <div class="detail-inquiry-header">
+          <h3>입찰하기</h3>
+          <button type="button" class="detail-inquiry-close" @click="closeBidModal">×</button>
+        </div>
+
+        <div class="detail-inquiry-summary">
+          <img :src="props.assets.listWatchImage" :alt="item.title" class="detail-inquiry-thumb" />
+          <div class="detail-inquiry-summary-copy">
+            <strong>{{ item.title }}</strong>
+            <span>현재 입찰가</span>
+            <em>{{ item.price }}원</em>
+          </div>
+        </div>
+
+        <div class="detail-bid-modal-body">
+          <div class="detail-bid-meta-row">
+            <span>입찰 단위</span>
+            <strong>{{ item.bidUnit }}원</strong>
+          </div>
+
+          <label class="detail-bid-modal-label">입찰 금액</label>
+
+          <label class="detail-bid-input-row">
+            <input
+              v-model="bidAmount"
+              type="text"
+              :placeholder="`최소 ${formatAmount(minimumBidAmount)}`"
+            />
+            <span>원</span>
+          </label>
+
+          <div class="detail-bid-quick-actions">
+            <button type="button" class="detail-bid-quick-chip" @click="stepBid(-1)">- {{ item.bidUnit }}</button>
+            <button type="button" class="detail-bid-quick-chip" @click="stepBid(1)">+ {{ item.bidUnit }}</button>
+          </div>
+        </div>
+
+        <div class="detail-bid-modal-actions">
+          <button type="button" class="detail-buy-now-button" @click="buyNow">
+            <span>{{ formatAmount(buyNowAmount) }}원 으로</span>
+            <strong>즉시 구매</strong>
+          </button>
+          <button type="button" class="detail-bid-submit-button" @click="submitBid">
+            입찰하기
+          </button>
+        </div>
+      </section>
     </div>
 
     <div v-if="isReportModalOpen" class="detail-inquiry-overlay" @click.self="closeReportModal">
